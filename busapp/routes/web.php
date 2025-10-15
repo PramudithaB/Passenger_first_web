c5fab4a459cdb94a90cf0c02dbe2427daf2ce79e
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2025-10-15 19:23:20 +0530

    .

--- busapp/routes/web.php
+++ busapp/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('travelor.travelor.homepage');
 });
 
 Route::get('/dashboard', function () {
