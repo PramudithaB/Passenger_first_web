641c68eea5cbd088eb11ad1a39a9169c92a974cf
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2025-12-24 16:18:37 +0530

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,4 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
@@ -44,4 +42,3 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
